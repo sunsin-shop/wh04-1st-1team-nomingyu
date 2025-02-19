9f410b870d69ab66ec29ecdedc4d88fb0ea6946b
jacob-53 <jacob8753@gmail.com>
2025-02-19 15:20:50 +0900

    v0.2

--- pages/4_Shoes.py
+++ pages/4_Shoes.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-
+from streamlit_calendar import calendar
 
 st.set_page_config(page_title="Shoes Analysis", page_icon = "🔍")
 #st.page_link("Main.py", label="Back to Main", icon="🏠")
